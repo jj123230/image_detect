bfc8b2536dbcaca1cc18ef9c06dd49fc14524f80
jj123230 <42804892+jj123230@users.noreply.github.com>
2025-04-29 16:41:10 +0800

    Add files via upload

---
+++ setting_config.py
@@ -0,0 +1,11 @@
+# 需要依照伺服器更改的資料
+test = 0 #線上需改成0才會讀取這裡的設定
+ip = ## your host_ip
+port = ## your port_ip
+
+env = ## 'test', 'prod' Revise it with the corresponding DB address as well
+DB_info = {
+        'uid': ## your sql uid,
+        'pwd': ## your sql pwd,
+        'address': ## your sql address
+    }
